4c09327f875c9176d6d4a578022b2dc42dde3717
B-asmala <basmalabayoumi@gmail.com>
2025-01-10 00:14:21 +0200

    summing subtitles using groq

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const axios = require('axios');
+const Groq = require('groq-sdk');
 const { YoutubeTranscript } = require('youtube-transcript');
+const marked = require('marked');
+
+
+
 //سأعطيك مضمون فيديو معين. قم بتلخيصه في نقاط ولا تضيع اي من التفاصيل المهمة. أضف اكبر عدد من التفاصيل المأخوذة من الفيديو. لا تستخدم غير اللغة العربية
 dotenv.config({ path: './config.env' });
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 const app = express();
 
@@ -22,16 +28,21 @@ app.get('/wake', (req, res)=>{
 //this endpoint is for summarizing video, id of video is provided as a parameter
 app.get('/sum/:id', async (req, res)=>{
     let subtitles;
+    let summed;
     try {
         const subtitlesArr = await YoutubeTranscript.fetchTranscript(req.params.id);
         for (const i of subtitlesArr) {
             subtitles += (i.text + ' ');
         }
 
-        console.log(subtitles);
+        //summarize the subtitles
+        summed = await sumAI(subtitles);
+
+        //using marked to convert form md to html
+        summed = marked.parse(summed);
         res.status(200).json({
             success: true,
-            data: subtitles
+            data: summed
         });
         
         
@@ -46,3 +57,35 @@ app.get('/sum/:id', async (req, res)=>{
 app.listen(3000, ()=> {
     console.log("server running");
 })
+
+
+async function sumAI(subtitles) {
+    const now = Date.now();
+    let summed = "";
+    const chatCompletion = await groq.chat.completions.create({
+        "messages": [
+        {
+            "role": "system",
+            "content": "I will provide you with a video's subtitles. summarize them into general points without missing any details. "
+        },
+        {
+            "role": "user",
+            "content": subtitles}
+        ],
+        "model": "gemma2-9b-it",
+        "temperature": 1,
+        "max_tokens": 1024,
+        "top_p": 1,
+        "stream": true,
+        "stop": null
+    });
+
+    for await (const chunk of chatCompletion) {
+        //process.stdout.write(chunk.choices[0]?.delta?.content || '');
+        summed += (chunk.choices[0]?.delta?.content || '');
+    }
+    console.log("time:");
+    console.log(Date.now() - now);
+    return summed;
+    
+}
